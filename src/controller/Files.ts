8516f2fe94a81d8f948d2c2791e7eaa7db449893
david123456858 <juandavidperaltafuentes@gmail.com>
2024-02-07 18:31:41 -0500

    comelCase arreglado

--- src/controller/Files.ts
+++ src/controller/Files.ts
@@ -1,10 +1,10 @@
 import { Request,Response } from "express"
 import {} from "mongoose"
-
+import {config} from 'dotenv'
 
 import filesModels from "../model/modelsFiles"
 
-
+config()
 export const saveFile = async (req:Request, res:Response)=>{
     try {
         const originalname = req.file?.originalname
@@ -16,6 +16,7 @@ export const saveFile = async (req:Request, res:Response)=>{
         }
         const create = await filesModels.create(filsModels)
         console.log(create)
+        console.log(process.env.PASSWORD_TOKEN)
         res.status(200).json({data:"TODO OKAY"})
     } catch (error) {
         throw new Error    
