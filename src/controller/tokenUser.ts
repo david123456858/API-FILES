ddcda831325ccceba2d16b8e0efd6ca5d4aace85
david123456858 <juandavidperaltafuentes@gmail.com>
2024-11-12 06:33:12 -0500

    cosas se vienen

--- src/controller/tokenUser.ts
+++ src/controller/tokenUser.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express"
 
 import { tokenSing } from "../helpers/tokens-files";
 import { User } from "../model/user";
-import { token } from "morgan";
+
 
 
 export const setTokens = async (res:Response,req:Request) => {
